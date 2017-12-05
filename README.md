7928c06fdb119bb5f259ff977307a4e85b350270
Matthew Kipper <mattvkipper@gmail.com>
2017-12-04 22:39:28 -0700

    Full Day 2 solution

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This solution performs pretty well. My laptop (i5 dual-core 2.60GHz + 16GB RAM) 
 Just a note -- the command line interface takes a string input, so summing up a number in a file (e.g. the input from the AoC website) requires something like this:
 
 `./1_inverse_captcha $(cat input.txt)`
+
+## Day 2: Corruption Checksum
+Not much to say about this one. Input parsing is pretty fragile, but everything seems okay outside of that. Complexity is O(nm) for an nxm input, and I can't think of any way to really improve this.  
--- src/bin/2_corruption_checksum.rs
+++ src/bin/2_corruption_checksum.rs
@@ -2,6 +2,7 @@ use std::num;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::cmp;
 
 fn print_usage() {
     println!("Day 2: Corruption Checksum");
@@ -43,9 +44,7 @@ fn input_from_file(filename: String) -> Result<Input, InputError> {
 }
 
 fn parse_input(text: String) -> Result<Input, InputError> {
-
     let mut values: Input = Vec::new();
-
     for line in text.lines() {
         let mut row: Vec<u32> = Vec::new();
         for entry in line.trim().split('\t') {
@@ -56,16 +55,35 @@ fn parse_input(text: String) -> Result<Input, InputError> {
         }
         values.push(row);
     }
-
     Ok(values)
 }
 
+/// Calculate the checksum, provided a valid input matrix. 
 fn checksum(input: Input) -> u32 {
-    0
+    // The following approach is used:
+    //  1. map() each row from Vec<u32> -> (min: u32, max: u32)
+    //  2. Take the sum of (max - min) for all (min, max) tuples
+    input.iter().map( |row| {
+        row.iter().fold( 
+            (row.first().unwrap(), row.first().unwrap()), 
+            |(min, max), x| {
+                (cmp::min(min, x), cmp::max(max, x))
+            }
+        )
+    })
+    .fold(0, |acc, (min, max)| acc + max - min)
 }
 
+/// Calculates the "checksum" of an input matrix file, printing the
+/// result to stdout. 
+///
+/// The input file has the following format:
+///     1. Columns tab-separated (\t)
+///     2. Rows newline-separated (\n)
+///     3. Values are unsigned 32-bit integers
+/// 
+/// The checksum is defined as sum(max(row) - min(row)) for all rows. 
 fn main() {
-
     let input = get_input();
 
     match input {
@@ -85,5 +103,41 @@ fn main() {
             println!("Parsing Error: {:?}", err);
             std::process::exit(-1);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_error_non_numeric() {
+        assert!(parse_input(String::from("1\t2\t3\n4\ta\t5\n")).is_err());
+    }
+
+    #[test]
+    fn parse_error_negative() {
+        assert!(parse_input(String::from("1\t2\t3\n4\t5\t-2\n")).is_err());
+    }
+
+    #[test]
+    fn parse_success() {
+        let input = String::from("1\t2\t3\n9\t8\t7\n");
+        assert_eq!(
+            parse_input(input).unwrap(),
+            vec![ vec![1, 2, 3], vec![9, 8, 7]]
+        );
+    }
+
+    #[test]
+    fn checksum_nonzero() {
+        let input = vec![ vec![6, 5, 1], vec![3, 5, 8], vec![5, 1, 3]];
+        assert_eq!(checksum(input), 14);
+    }
+
+    #[test]
+    fn checksum_zero() {
+        let input = vec![ vec![1, 1], vec![2, 2], vec![99, 99]];
+        assert_eq!(checksum(input), 0);
     }
 }
